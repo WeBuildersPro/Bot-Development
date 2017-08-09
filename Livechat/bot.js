34d6fdd13b55f57a060916386f0e43cdd21ee3cb
Unknown <webuilderspro@gmail.com>
2017-08-09 10:54:02 -0400

    more testing

--- Livechat/bot.js
+++ Livechat/bot.js
@@ -2,19 +2,28 @@ const slackBot = require("slackbots");
 
 // Create the bot
 var bot = new slackBot({
-    token: 'xoxb-223984827124-y5kMDVAVSCATDACOG2BgLh4s', 
+    token: 'xoxb-223984827124-Mm5cgXC10SKfLukpvagfJ4Bq', 
     name: "Livechat Bot"
 });
 
 // When bot starts
 bot.on('start', function() {
+	console.log();
 	console.log("Bot is now running");
+
     var params = {
     };
-    var channelRaw = bot.getChannels();
-    for (i = 0; i < channelRaw.length; i++) {
-    	var channels = bot.getChannel(channelRaw[i]);
-    	console.log(channels);
-    }
- 
+    
+    var channelsRaw = bot.getChannels();
+    var channelsOut = bot.getChannel("general");
+    console.log(channelsOut);
+
+
+});
+
+bot.on('message', function(data) {
+    // all ingoing events https://api.slack.com/rtm 
+    console.log();
+    console.log("Data:");
+    console.log(data);
 });
